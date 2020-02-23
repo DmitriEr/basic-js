c514766955f57b4bbbe717da7c8f94c5e291f2a4
Дмитрий <tomountain@mail.ru>
2020-02-23 22:02:57 +0400

    feat: solution extended-repeater.js

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,33 +1,45 @@
-module.exports = function repeater(/*str, options*/) {        
-    throw "Not implemped"
-};
-  
-/*
-if (options.separator === undefined) {
-        options.separator = "+";
-        }
-        
-      if (options.additionSeparator === undefined) {
-        options.additionSeparator = "";
-        }
-       
-      if (options.repeatTimes === undefined) {
-        options.repeatTimes = 1;
-        }
-      
-      if (options.additionRepeatTimes === undefined) {
-        options.additionRepeatTimes = 1;
-        options.additionSeparator = ""
-        }
-      
+module.exports = function repeater(str, options) {        
+  // результат общий
+  let result = "";
+  // результат внутри addition
+  let secondStr = "";
+  // результат внутри addition - разделитель в конце строки
+  let letter = ""
+  // проверка на наличие внутренниего повтора
+  if (options.additionRepeatTimes === undefined) {
+    options.additionRepeatTimes = 1;
+    }
+  // цикл внутреннего повтора
+  for(let j = 0; j < options.additionRepeatTimes; j++) {
+      // проверка на наличие дополнительного слова
       if (options.addition === undefined) {
         options.addition = "";
-        }
-      
-      let test = (str + (options.addition + options.additionSeparator).repeat(options.additionRepeatTimes) + options.separator).repeat(options.repeatTimes);
-      
-      if (options.repeatTimes == 1) {
-        return test.slice(0, test.length - options.separator.length - options.additionSeparator.length);
-        }
-      return test.slice(0, test.length - options.separator.length);
-*/
+      };
+      // проверка на наличие разделителя дополнительного слова
+      if (options.additionSeparator === undefined) {
+        options.additionSeparator = "|";
+        };
+        // результат
+      secondStr += options.addition + options.additionSeparator;
+      };
+  // проверка на длину строки внутреннюю
+  if (secondStr.length > 0) {
+    letter = secondStr.slice(0, secondStr.length - options.additionSeparator.length)
+  }
+  // проверка на наличие повтора
+  if (options.repeatTimes === undefined) {
+    options.repeatTimes = 1;
+    }
+  // цилк общий
+  for (let i = 0; i < options.repeatTimes; i++) {
+    // проверка на разделитель
+    if (options.separator === undefined) {
+        options.separator = "+";
+      };
+    // результат
+    result += str + letter + options.separator;
+    }
+  // результат - последний разделитель
+  return result.slice(0, result.length - options.separator.length);
+
+};
