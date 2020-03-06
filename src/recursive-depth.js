05f3f077a921584d732ac117216ed30c0869ebd9
Дмитрий <tomountain@mail.ru>
2020-03-06 21:07:24 +0400

    feat: solution recursive-depth

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,6 +1,17 @@
 module.exports = class DepthCalculator {
-    calculateDepth(/* arr */) {
-        throw 'Not implemented';
-        // remove line with error and write your code here
-    }
-};
+  calculateDepth(arr) {
+  // Если arr не массив возвращаем 0
+  if (!Array.isArray(arr)) return 0;
+  // Объевляем переменную счетчик
+  let depth = 0;
+  // Проверяем каждый элемент на массив
+  arr.forEach(a => {
+    if (Array.isArray(a)) {
+        // Находим максимально вложенный элемент
+        depth = Math.max(this.calculateDepth(a), depth);
+        }
+    })
+    // Добавляем arr
+    return depth + 1
+  }
+}
