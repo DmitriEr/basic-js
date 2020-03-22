d41d1a2c3445a041c38a5a89ecd3c3167c110448
Дмитрий <tomountain@mail.ru>
2020-03-22 22:06:23 +0400

    feat: solution vigenere-cipher and transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,33 +1,5 @@
 module.exports = function transform(arr) {
-let doubleNext = arr.findIndex(a => a == "--double-next");
-let doublePrev = arr.findIndex(a => a == "--double-prev");
-let discardNext = arr.findIndex(a => a == "--discard-next");
-let discardPrev = arr.findIndex(a => a == "--discard-prev");
- 
- for (let i = 0; i < arr.length; i++) {
-
-  if (arr[i] == "--double-next") {
-    doubleNext < arr.length - 1 ? arr.splice(doubleNext, 1, arr[i + 1]) : arr.splice(doubleNext, 1);
-    }
-    
-  if (arr[i] == "--double-prev") {
-   doublePrev > 0 ? arr.splice(doublePrev, 1, arr[i - 1]) : arr.splice(doublePrev, 1);
-   }
-
-  if (arr[i] == "--discard-next") {
-    discardNext <= arr.length - 1 ? arr.splice(discardNext, 2) : arr.splice(discardNext, 1);
-    }
-    
-  if (arr[i] == "--discard-prev") {
-    discardPrev > 0 ? arr.splice(discardPrev - 1, 2) : arr.splice(discardPrev, 1)
-    }
-    
-  }     
-   return arr
- };
-
-/*
-if (!Array.isArray(arr)) throw new Error([]);
+  if (!Array.isArray(arr)) throw new Error([]);
 
   let doubleNext = arr.findIndex(a => a == "--double-next");
   let doublePrev = arr.findIndex(a => a == "--double-prev");
@@ -38,20 +10,19 @@ if (!Array.isArray(arr)) throw new Error([]);
  
    if (arr[i] == "--double-next" && doubleNext < arr.length - 1) {
      arr.splice(doubleNext, 1, arr[i + 1]);
-     }
+     };
      
    if (arr[i] == "--double-prev" && doublePrev > 0) {
     arr.splice(doublePrev, 1, arr[i - 1]);
-    }
+    };
     
    if (arr[i] == "--discard-next" && discardNext < arr.length - 1) {
      arr.splice(discardNext, 2)
-     }
+     };
      
    if (arr[i] == "--discard-prev" && discardPrev > 0) {
      arr.splice(discardPrev - 1, 2)
-     }
-     
+     };
    }     
-   return arr
-   */
+   return arr;
+};
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,12 +1,66 @@
+// массив с алфавитом
+const alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    encrypt(str, key) {
+        // массив для результата
+        let arr = [];
+        // счетчик
+        let count = 0;
+        // цикл строки
+        for (let i = 0; i < str.length; i++) {
+          // если значение - латинская буква
+          if (/[A-Za-z]/.test(str[i])) {
+            // номер буквы в алфавите для строки
+            let strCode = str[i].toUpperCase().charCodeAt() - 65;
+            // номер буквы в алфавите для ключа
+            let keyCode = key[count % key.length].toUpperCase().charCodeAt()-65;
+            // сумма. Результат буквы из массива с алфавитом. Если сумма больше длины алфавита. Тогда берется индекс буквы (остаток от деления на длину алфавита)
+            let result = alphabet[(strCode + keyCode) % 26];
+            // записываем в новый массив значение
+            arr[i] = result;
+            // счетчик повторяет ключ. если длина ключа меньше длины строки
+            count++;
+            } else {
+              // если значение другое (не латинская буква). Записывается, как есть
+              arr[i] = str[i];
+              }
+          }
+        //   вывод строку
+      return arr.join("");  
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    decrypt(message, key) {
+        // массив для результата
+        let arr = []
+        // счетчик
+        let count = 0;
+        // цикл сообщения
+        for (let i = 0; i < message.length; i++) {
+            // если значение - латинская буква
+            if (/[A-Za-z]/.test(message[i])) {
+                // номер буквы в алфавите для сообщения
+                let messageLetter = message[i].toUpperCase().charCodeAt() - 65;
+                // номер буквы в алфавите для ключа
+                let keyLetter = key[count % key.length].toUpperCase().charCodeAt() - 65;
+                // сумма. Результат буквы из массива с алфавитом. При отрицательном числе messageLetter - keyLetter прибавляем длину алфавита
+                let result;
+                if (messageLetter - keyLetter >= 0) {
+                    result = alphabet[(messageLetter - keyLetter) % 26];
+                } else {
+                    result = alphabet[(messageLetter - keyLetter + 26) % 26];
+                }
+                // записываем в новый массив значение
+                arr[i] = result;
+                // счетчик повторяет ключ. если длина ключа меньше длины строки
+                count++;
+            } else {
+                // если значение другое (не латинская буква). Записывается, как есть
+                arr[i] = message[i];
+            }
+        }
+        // Вывод строки
+        return arr.join("")
     }
 }
 
